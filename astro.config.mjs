13f459c7e90af78ee99b1dd9319a1d19d2d36722
ManuelGil <atencion.ms@outlook.com>
2025-05-03 21:57:47 -0500

    feat: 🚧 add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
-
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-		server: {
-			allowedHosts: ['.trycloudflare.com'],
-		},
-	},
-
-	integrations: [react()],
+  site: "https://json-flow.com",
+  base: "/",
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      allowedHosts: [".trycloudflare.com"],
+    },
+  },
+  integrations: [react()],
 });
